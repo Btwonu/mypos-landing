a83adebe3022c69e39b7d58f6b8e8f3e9f32d572
vkdelchev <btwonu@gmail.com>
2024-10-10 13:51:23 +0300

    config: add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
 	root: resolve(__dirname, 'src'),
@@ -7,5 +8,10 @@ export default defineConfig({
 	build: {
 		outDir: resolve(__dirname, 'dist'),
 		assetsInlineLimit: 0,
+	},
+	css: {
+		postcss: {
+			plugins: [autoprefixer({})],
+		},
 	},
 });
